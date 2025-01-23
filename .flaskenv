22cb0d9cb209d75168eb238f2217bcb52253b687
M-AliHamza <mirza.malihamza@gmail.com>
2025-01-23 22:34:34 +0500

    add user profile and error handling

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,4 @@
 FLASK_APP=microblog.py
 FLASK_ENV=development
 SECRET_KEY='your_secret_key'
+FLASK_DEBUG=1
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
+from logging.handlers import RotatingFileHandler
+import logging
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 app.config.from_object(Config)
@@ -11,4 +14,17 @@ login.login_view = 'login'
 db=SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes , models
+from app import routes , models , errors
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                    backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
